05ad17b634d090205798611e9396c41fc60463a7
Chris Grams <chrisagrams@gmail.com>
2024-01-29 14:25:01 -0600

    Docker compose example

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 from models import Base, User
-
+import redis
 
 app = FastAPI()
+
+redis_host = "redis"
+redis_port = 6379
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0)
 
 DATABASE_URL = "sqlite:///./db/test.db"
 engine = create_engine(DATABASE_URL)
@@ -32,19 +36,28 @@ async def create_user(name: str):
             user = User(name=name)
             session.add(user)
             session.commit()
-            session.close()
-            return {"message": f"User {name} created"}
+
+        redis_client.set(name, name)
+
+        return {"message": f"User {name} created"}
     except SQLAlchemyError:
         raise HTTPException(status_code=500, detail="Database Error")
 
 
 @app.get("/user/{name}")
 async def get_user(name: str):
+    cached_user = redis_client.get(name)
+    if cached_user:
+        return {"message": cached_user.decode('utf-8')}
+
     try:
         with SessionLocal() as session:
             user = session.query(User).filter(User.name == name).first()
             if user is None:
                 raise HTTPException(status_code=404, detail="User not found")
-            return user
+
+        redis_client.set(name, name)
+
+        return user
     except SQLAlchemyError:
         raise HTTPException(status_code=500, detail="Database Error")
